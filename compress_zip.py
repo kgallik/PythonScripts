4423d230d67cb42d6f6d81b770a3a1ee3dc64576
Kristin Gallik <kristin.gallik@gmail.com>
2026-06-04 12:01:28 +0300

    created script to compress files and preserve relative dirs

---
+++ compress_zip.py
@@ -0,0 +1,33 @@
+import os
+import zipfile
+from tqdm import tqdm
+
+def write_zip(output_zip, dir):
+    # Create a ZIP file
+    with zipfile.ZipFile(output_zip, 'w', zipfile.ZIP_DEFLATED) as zipf:
+        for root, _, files in os.walk(dir):
+            for file in files:
+                if 'avg' in file:
+                    # Create a complete file path
+                    file_path = os.path.join(root, file)
+                    # Add file to the ZIP file
+                    zipf.write(file_path, os.path.relpath(file_path, os.path.join(dir, '..')))
+                elif 'masks' in file:
+                    # Create a complete file path
+                    file_path = os.path.join(root, file)
+                    # Add file to the ZIP file
+                    zipf.write(file_path, os.path.relpath(file_path, os.path.join(dir, '..')))
+
+
+def parse_arguments():
+    import argparse
+    parser = argparse.ArgumentParser(description='Compress files from subdirectories into a zip with directories preserved')
+    parser.add_argument('--source_folder', type=str, help='Parent directory')
+    parser.add_argument('--zip_name', type=str, help='name of zip file')
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    arg = parse_arguments()
+    output_zip = arg.zip_name
+    source_folder = arg.source_folder
+    write_zip(output_zip,source_folder)
